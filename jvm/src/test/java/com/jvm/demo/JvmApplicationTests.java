8ba6e9b123b1ebe5e1604b71c17241ac869277bf
rvenera <ricarte.venerayan@toronto.ca>
2022-12-12 22:15:57 -0500

    first commit

---
+++ jvm/src/test/java/com/jvm/demo/JvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
